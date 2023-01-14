5e17b0bd81d0b6bb5498635153465ba2caf01dd8
Marie Meisters <mariemeisters84@gmail.com>
2023-01-14 11:01:08 +0100

    Ajout fonction useLocation header

--- kasa/src/components/Header/index.jsx
+++ kasa/src/components/Header/index.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/Logo.svg';
 import './style.scss';
 
 
 const Header = () => {
+
+    let location = useLocation();
+
     return (
         <header>
                 <div className='header__logo'><Logo/></div>
                 <nav className='header__nav'>
                     <ul>
-                        <li><Link to="/">Accueil</Link></li>
-                        <li><Link to="/a-propos">A Propos</Link></li>
+                        <li><Link to="/" className={location.pathname === '/' ? 'active' : ''}>
+                            Accueil
+                            </Link>
+                        </li>
+                        <li><Link to="/a-propos" className={location.pathname === '/a-propos' ? 'active' : ''}>
+                            A Propos
+                            </Link>
+                        </li>
                     </ul> 
                 </nav>
         </header>
