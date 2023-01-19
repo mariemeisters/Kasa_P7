a0dafa40d0bbc472c23d9e2af7f036526ad2554f
Marie Meisters <mariemeisters84@gmail.com>
2023-01-19 14:40:43 +0100

    Finalisation header footer

---
+++ kasa/src/components/Banner/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ReactComponent as Logo } from '../../assets/Logo.svg';
+import './style.scss';
+
+
+const Banner = () => { 
+ 
+    return ( 
+        <section>
+              
+        </section>
+    );
+};
+
+export default Banner; 
